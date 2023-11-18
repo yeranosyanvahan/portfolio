da828920a5e451a1a6dfe043d397117406fd6c18
yeranosyanvahan <yeranosyanvahan@gmail.com>
2023-11-18 14:05:50 +0400

    add mobile responsiveness

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ export default class Contact extends React.Component {
   render() {
     return (
         <Layout><div className='content centered gapped'>
-            <div style={{"display":"flex", "gap":"4rem"}}>
+            <div className='contactpage'>
               <form className="contact" target="_self" action="https://formsubmit.co/formsubmit@saghatelyaninstitute.org" method="POST">
                 <h2 className="wide">Contact Form</h2>
                 <input type="hidden" name="_next" value="https://www.yeranosyanvahan.com/contact" />
